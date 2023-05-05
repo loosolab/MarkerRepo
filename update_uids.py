c5ac793c95e3fc28ad596bd24d0b32e2752c5ce2
Micha Frederick Keßler <Micha-Frederick.Kessler@mpi-bn.mpg.de>
2023-05-05 11:57:30 +0200

    update_uids.py added n added new testdata added

---
+++ update_uids.py
@@ -0,0 +1,67 @@
+import os
+import re
+
+def get_all_paths_and_uids(path):
+    """
+    Get all files and UIDs in the given path, including subdirectories.
+
+    Parameters
+    ----------
+    path : str
+        The path to the folder containing the marker lists.
+
+    Returns
+    -------
+    dict :
+        A dictionary with the UIDs as keys and lists of file paths as values.
+    """
+    files_with_uids = {}
+    pattern = re.compile(r"_([0-9]+)\.yaml$")
+    for root, _, files in os.walk(path):
+        for file in files:
+            if file.endswith(".yaml"):
+                match = pattern.search(file)
+                if match:
+                    uid = int(match.group(1))
+                    file_path = os.path.join(root, file)
+                    if uid in files_with_uids:
+                        files_with_uids[uid].append(file_path)
+                    else:
+                        files_with_uids[uid] = [file_path]
+
+    print(files_with_uids)
+    return files_with_uids
+
+
+def update_uids(repo_lists_path):
+    """
+    Update the UIDs of the files in the given path to ensure uniqueness.
+
+    Parameters
+    ----------
+    repo_lists_path : str
+        The path to the folder containing the marker lists.
+
+    """
+    files_with_uids = get_all_paths_and_uids(repo_lists_path)
+    uids_in_use = set(files_with_uids.keys())
+
+    for uid, file_paths in files_with_uids.items():
+        if len(file_paths) > 1:
+            file_paths.sort(key=os.path.getmtime)
+            for file_path in file_paths[1:]:
+                new_uid = uid
+                while new_uid in uids_in_use:
+                    new_uid += 1
+                uids_in_use.add(new_uid)
+                new_file_path = re.sub(r"_([0-9]+)\.yaml$", f"_{new_uid}.yaml", file_path)
+                os.rename(file_path, new_file_path)
+                print(f"File '{file_path}' was renamed to '{new_file_path}'")
+
+
+def main():
+    lists_path = "lists"
+    update_uids(lists_path)
+
+if __name__ == "__main__":
+    main()
