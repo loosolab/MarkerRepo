5d23d3264cf1666d7df2e46ab685b7177c8cf6d3
Micha Frederick Keßler <Micha-Frederick.Kessler@mpi-bn.mpg.de>
2023-05-24 11:14:01 +0200

    guided_search() implemented ngs

--- marker_repo.py
+++ marker_repo.py
@@ -95,6 +95,57 @@ def search_db(df, keywords, case_sensitive=False, exact=False):
     return filtered_df
 
 
+def guided_search(REPO_LISTS_PATH, df=None):
+    """
+    An interactive function that guides the user through the process of searching the DataFrame.
+
+    Parameters
+    ----------
+    REPO_LISTS_PATH : str
+        The path where the lists of the Marker Repo are stored - probable 'REPO_PATH/lists'.
+    df : pd.DataFrame, default: None
+        The DataFrame to search in. If not provided, the function will create one from the REPO_LISTS_PATH.
+
+    Returns
+    -------
+    pd.DataFrame
+        The DataFrame that contains the search results.
+    """
+    
+    # Get the DataFrame if not provided
+    if df is None:
+        df = get_db(REPO_LISTS_PATH)
+
+    # Print column names for user's reference
+    print("Available columns for search:")
+    print('|'.join(df.columns.tolist()))
+    
+    col_to_search = input("Enter column to search in (leave blank to search in all columns): ")
+    search_term = input("Enter search term: ")
+    exact = input("Perform an exact search? (yes/no): ").lower() == "yes"
+    case_sensitive = input("Consider case sensitivity? (yes/no): ").lower() == "yes"
+    
+    # Build the keywords
+    if col_to_search:
+        keywords = {col_to_search: search_term}
+    else:
+        keywords = search_term
+
+    # Perform the search
+    results = search_db(df, keywords, exact=exact, case_sensitive=case_sensitive)
+
+    # Display the results
+    display(results)
+
+    # Further filtering?
+    further_filter = input("Do you want to filter the results further? (yes/no): ").lower() == "yes"
+
+    if further_filter:
+        return guided_search(REPO_LISTS_PATH, results)
+    
+    return results
+
+
 def flatten_dict(d, parent_key='', sep='_', list_sep='\n'):
     """
     Flatten a nested dictionary, concatenating keys with a separator.
