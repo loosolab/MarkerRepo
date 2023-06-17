7fafae74587552a482fccdaabab74ffff0660465
Micha Frederick Keßler <Micha-Frederick.Kessler@mpi-bn.mpg.de>
2023-06-17 17:06:56 +0200

    check_uids.py prints fixed

--- markerrepo/check_uids.py
+++ markerrepo/check_uids.py
@@ -48,9 +48,10 @@ def check_uid(new_uid, repo_lists_path="./lists"):
     """
 
     uids = get_all_uids(repo_lists_path=repo_lists_path)
-    print(new_uid, type(new_uid))
-    print(uids)
-    print(uids[0], type(uids[0]))
+    
+    print(f"Your UID: {new_uid}")
+    print(f"All UIDs: {uids}")
+
     if new_uid in uids:
         print(f"Duplicate UID found: {new_uid}")
         return False
