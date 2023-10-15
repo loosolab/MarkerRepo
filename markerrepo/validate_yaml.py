e2903416cff2b22fe98757c4d3f925a76decadec
Micha Frederick Keßler <Micha-Frederick.Kessler@mpi-bn.mpg.de>
2023-10-15 15:34:00 +0200

    marker_repo error handling implemented, metadata organizer code marked

--- markerrepo/validate_yaml.py
+++ markerrepo/validate_yaml.py
@@ -1,3 +1,6 @@
+# This code is based on the GitLab repository "metadata organizer", which can be found at the following URL:
+# https://gitlab.gwdg.de/loosolab/software/metadata-organizer
+
 import os.path
 import datetime
 from .utils import read_in_yaml, find_keys, get_whitelist, read_whitelist, read_grouped_whitelist
